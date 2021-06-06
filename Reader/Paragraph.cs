a94d71c3547ed80e9da95e38afa28ff74d22ccaa
baizz-maker <55344134+baizz-maker@users.noreply.github.com>
2021-06-06 20:08:34 +0800

    Add files via upload

---
+++ Reader/Paragraph.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordTree.Model;
+
+namespace Reader
+{
+    class Paragraph
+    {   
+        public string ParagraphName { get; set; }//文段名称
+        public List<Word> KeyWords { get; set; }// 重点单词
+        public Word SelectedWord { get; set; }// 被选中的词
+        public string content { get; set; }// 文段内容
+
+        public Paragraph(string name)
+        {
+            ParagraphName = name;
+        }
+        public Paragraph(){}
+    }
+}
---
+++ Reader/UserTextChoice.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reader
+{
+    public delegate void GetChoice(string choice);
+    public delegate void Tracecommand();
+    public partial class UserTextChoice :  Form
+    {
+        ParagraphManager manager = new ParagraphManager();
+        List<string> userchoices = new List<string>();
+
+        private GetChoice choice;
+        private Tracecommand trace;
+        public UserTextChoice(GetChoice Setpara, Tracecommand command)
+        {
+            InitializeComponent();
+            userchoices = manager.GetAllParagraghNames();
+            choice_comboBox_Searcher.Items.AddRange(userchoices.ToArray());
+            choice = Setpara;
+            trace = command;
+        }
+
+        private void confirm_button_Click(object sender, EventArgs e)
+        {
+            choice(choice_comboBox_Searcher.Text);
+            trace();
+            this.Close();
+        }
+
+        private void choice_comboBox_Searcher_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            choice_comboBox_Searcher.Text = (string)choice_comboBox_Searcher.SelectedItem;
+            
+        }
+    }
+}
